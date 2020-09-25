d09c20b2bd32e18d14398e4ffb6833d25b615776
romlm <romain103paris@gmail.com>
2020-09-25 09:53:46 +0200

    initial ts migration commit

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-console.log('hello, world!')
+import express, {Express, Request, Response} from 'express';
+
+const app: Express = express();
+
+app.listen(8080);
+app.get('/', (req: Request, res: Response<string>) => {
+	res.status(200);
+	res.write("Hello, World!");
+	res.end();
+})
